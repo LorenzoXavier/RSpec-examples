03d82b149c5dc36febb82ae596f02b271169792b
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-07-21 14:23:54 +0100

    School challenge all green and refactored

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_21_120348) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_130223) do
   create_table "cards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schools", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/models/school_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+
+RSpec.describe School do
+  it 'has a name' do
+    school = School.new('Beverly Hills High School')
+    expect(school.name).to eq('Beverly Hills High School')
+  end
+
+  it 'should start off with no students' do
+    school = School.new('Notre Dame High School')
+    expect(school.students).to eq([])
+  end
+end
