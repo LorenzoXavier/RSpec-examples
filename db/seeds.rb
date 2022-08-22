79d587da693aeb2f3eb9aa1318ed8e505669cae7
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-08-22 19:30:59 +0100

    removed body:text and added has_action_text :body and all the correct files to make this work. Worked on the seed file and index view to make frontend nicer

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,16 @@ tile = Tile.new(
   title: "Card model RSpec checks",
   model_link: "https://github.com/LorenzoXavier/RSpec-examples/blob/main/app/models/card.rb",
   spec_link: "https://github.com/LorenzoXavier/RSpec-examples/blob/main/spec/models/card_spec.rb",
-  body: "Run rake to see this working"
+  body: "<p>This has a mixture of techniques including</p>
+    <ul>
+      <li>Before block</li>
+      <li>Defining and calling methods within test</li>
+      <li>Let helper method</li>
+      <li>Custom error messages</li>
+    </ul>
+    <p>To run the spec file for the Card model, run: <br>
+    <strong>$ rspec ./spec/models/card_spec.rb</strong></p>
+  "
 )
 puts "Tile created: '#{tile.title}'"
 tile.save!
