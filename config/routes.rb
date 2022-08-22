775d161460b169032d1a6dcc29594ca87547e42a
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-08-22 12:36:00 +0100

    added bootstrap to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   get 'pages/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#index"
 end
