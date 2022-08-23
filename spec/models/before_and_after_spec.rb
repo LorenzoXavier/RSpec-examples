947f6d012de552d7e4510eaa5c1465f1d79f372b
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-08-23 12:19:23 +0100

    using :context and :example before and after hooks

---
+++ spec/models/before_and_after_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe 'before and after hooks' do
+
+
+  before(:example) do # run before each example
+    puts "Before example"
+  end
+
+  after(:example) do # run after each example
+    puts "after example"
+  end
+
+  before(:context) do # run one time only, before all of the examples in a group
+    puts "Before context"
+  end
+
+  after(:context) do # run one time only, after all of the examples in a group
+    puts "after context"
+  end
+
+  it 'is just a random example' do
+    expect(5 * 4).to eq(20)
+  end
+
+  it 'is just a anothe random example' do
+    expect(3-2).to eq(1)
+  end
+end
