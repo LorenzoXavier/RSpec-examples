1de61405ab5a0b6b2c4ebaf28046bf6c2a908756
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-08-29 17:33:58 +0100

    working with the fantastic satisy matcher, which allows you to custom error message and enter what needs to be satisfied in a block. Seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,21 @@ Tile.destroy_all
 puts "All previous tiles have been removed"
 
 tile = Tile.new(
+  title: "satisfy matcher",
+  model_link: "",
+  spec_link: "https://github.com/LorenzoXavier/RSpec-examples/blob/master/spec/models/satisfy_matcher_spec.rb",
+  body: "<p>
+  This fantastic mathcher gives you the anility to custom an error message by putting a string in parenthesis after calling satisfy()<br>
+  You then enter a do block to define what needs to be satisfied
+  <br>To run the spec file for the overwriting_let model, run: <br>
+    <strong>$ rspec ./spec/models/satisfy_matcher_spec.rb</strong>
+  </p>
+  "
+)
+puts "Tile created: '#{tile.title}'"
+tile.save!
+
+tile = Tile.new(
   title: "respond_to matcher",
   model_link: "",
   spec_link: "https://github.com/LorenzoXavier/RSpec-examples/blob/master/spec/models/respond_to_matcher_spec.rb",
